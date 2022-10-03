adaf121f80e0782d5501da6176b7bb476f78b229
ansary29 <thamimansary29@gmail.com>
2022-10-03 11:25:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="dashboard">
+     <BrowserRouter>
+     <Routes>      
+         <Route path="/" element={<Register />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/profile" element={<Navbar />} />
+     </Routes>
+     </BrowserRouter>
+     <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
